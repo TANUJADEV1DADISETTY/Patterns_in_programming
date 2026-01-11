9af936f77d24fadba21d145be630d817d69a624c
TANUJADEVIDADISETTY <dtanuja3011@gmail.com>
2026-01-11 12:44:13 +0530

    Pattern1

---
+++ pattern1.java
@@ -0,0 +1,28 @@
+// Patterns
+//1. Pattern 1
+/*
+    * * * * *
+    * * * * *
+    * * * * *
+    * * * * *
+    * * * * *
+*/
+
+import java.util.*;
+
+public class pattern1 {
+    public static void pattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pattern(n);
+    }
+}
